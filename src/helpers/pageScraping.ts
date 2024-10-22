a8d725b85b980ca1985a44097762ab8d2f7ef1f4
Ivan M <ivan.martyushev@loblaw.ca>
2024-10-22 18:42:36 -0400

    fix: game title scrape

--- src/helpers/pageScraping.ts
+++ src/helpers/pageScraping.ts
@@ -14,7 +14,7 @@ export async function fetchAndScrapeUrl(url: string): Promise<WishlistItem> {
 	try {
 		const res = await fetch(url)
 		const html = await res.text()
-		const title = pickBetween(html, 'data-qa="mfe-game-title#name">', '</h1>') || pickBetween(html, '<meta name="searchtitle" content="', '"/>')
+		const title = pickBetween(html, 'data-qa="mfe-game-title#name">', '</h1>') || pickBetween(html, '<h1 class="game-title">', '</h1>')
 		const price = pickBetween(html, '"discountedPrice":"', '",')
 		const ogPrice = pickBetween(html, '"originalPrice":"', '",') || ''
 		const saleEnds = pickBetween(html, 'data-qa="mfeCtaMain#offer0#discountDescriptor" class="psw-c-t-2">', '</span>') || ''
